500767bc652d6b8206a2a269d7c5c1afbfef92a7
chuotbachweb <chuotsekiro@gmail.com>
2024-05-22 21:16:43 +0700

    upgrade version expo

--- src/utils/httprequest.js
+++ src/utils/httprequest.js
@@ -2,5 +2,5 @@ import axios from "axios";
 console.log(process.env.REACT_APP_BASE_URL);
 export const httpRequest = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "http://192.168.1.6:1407/api/",
+  baseURL: "http://192.168.1.2:1407/api/",
 });
